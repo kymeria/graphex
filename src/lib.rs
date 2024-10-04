70905b774fcacab3ab0df7a4b9e510b8d305eabb
Matthieu Gautier <mgautier@kymeria.fr>
2024-10-04 19:44:19 +0200

    Add documentation on `serde` features

--- src/lib.rs
+++ src/lib.rs
@@ -8,6 +8,22 @@
 //! - [Display] is for display the final node at end of exploration.
 //!
 //! Few other strucs and functions are defined as helper.
+//!
+//! ## Features
+//!
+//! "Serde" support can be added using `serde` feature.
+//! It add the `Node::serde` method to get a serde value from a [Node].
+//!
+//! To avoid any compilation error when a third party project activate the `serde` feature on
+//! a project which has not implemented `Node::serde`, `Node::serde` returns a `Option` and a
+//! default implementation returning `None` is provided.
+//!
+//! However, providing a default implementation prevent the compiler to fail if someone forget to
+//! implement `Node::serde`. The feature `serde_no_defaul_impl` do not provide a default
+//! implementation and so compiler fails if `Node::serde` is not implemented.
+//!
+//! !! `serde_no_default_impl` MUST NOT be activated in `Cargo.toml`. Activate it only temporarly
+//! from command line `cargo build --features=serde_no_default_impl` !
 
 mod display;
 mod error;
